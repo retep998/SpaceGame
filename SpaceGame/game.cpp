6ed9f25b4d7caa11f01b084301bdadb704e3273f
retep998 <retep998@verizon.net>
2013-11-27 05:19:05 -0500

    Adding basic game stuff
    Signed-off-by: retep998 <retep998@verizon.net>

---
+++ SpaceGame/game.cpp
@@ -0,0 +1,41 @@
+//////////////////////////////////////////////////////////////////////////////
+// SpaceGame - A game that takes place in space                             //
+// Copyright © 2013 Peter Atashian                                          //
+//                                                                          //
+// This program is free software: you can redistribute it and/or modify     //
+// it under the terms of the GNU Affero General Public License as           //
+// published by the Free Software Foundation, either version 3 of the       //
+// License, or (at your option) any later version.                          //
+//                                                                          //
+// This program is distributed in the hope that it will be useful,          //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of           //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            //
+// GNU Affero General Public License for more details.                      //
+//                                                                          //
+// You should have received a copy of the GNU Affero General Public License //
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.    //
+//////////////////////////////////////////////////////////////////////////////
+
+#include "game.hpp"
+#include <GLFW/glfw3.h>
+
+namespace space {
+    namespace game {
+        bool over = false;
+        void init() {
+
+        }
+        void unload() {
+
+        }
+        void loop() {
+
+        }
+        void play() {
+            init();
+            while (!over)
+                loop();
+            unload();
+        }
+    }
+}
